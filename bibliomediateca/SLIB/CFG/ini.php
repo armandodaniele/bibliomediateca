dda8108dca8d6ee7bac0aa389e12e58505893570
tecnici <tecnici@TT07>
2016-12-30 11:56:52 +0100

    file config

--- bibliomediateca/SLIB/CFG/ini.php
+++ bibliomediateca/SLIB/CFG/ini.php
@@ -1,3 +1,13 @@
 <?php 
-
+$DB_host='localhost';
+$DB_name='bibliomediateca';
+$DB_user='root';
+$DB_pass='';
+$codice = "";
+$user = "admin";
+$passwordMD5 = "admin";
+$INFO_email = "biblio@aixt.net";
+$INFO_dominio = "www.aixt.net";
+$TEC_email = "bibliomediateca@aixt.net";
+$KeyPub = '';
 ?>
